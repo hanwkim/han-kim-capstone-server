b2ed0762160c756fc3b08dd40d906c1b2e54f597
Han Kim <hanwkim88@gmail.com>
2023-02-08 01:28:56 -0500

    Adjust results route to request listings from different cities

--- controllers/resultsController.js
+++ controllers/resultsController.js
@@ -8,31 +8,42 @@ const API_KEY = process.env.API_KEY;
 const characterList = JSON.parse(fs.readFileSync("./data/characters.json"));
 
 const getResults = (req, res) => {
+	const winningFood = characterList[0].find(
+		(character) => character.name === req.query.winner
+	);
 
-    const winningFood = characterList[0].find(character => character.name === req.query.winner);
-	function sendResults(results) {
-		res.json(results);
-	}
-
-	function sendError(error) {
-		res.status(404).send("Unable to find any listings");
-	}
-
-	client
+	if (req.query.city === "current") {
+		client
+			.textSearch({
+				params: {
+					query: req.query.winner,
+					location: req.query.location,
+					radius: 5000,
+					key: API_KEY,
+				},
+			})
+			.then((response) => {
+				res.json([winningFood, response.data.results]);
+			})
+			.catch((error) => {
+				res.status(404).send("Unable to find listings");
+			});
+	} else {
+		client
 		.textSearch({
 			params: {
-				query: req.query.winner,
-				location: req.query.location,
+				query: `${req.query.winner} in ${req.query.city}`,
 				radius: 5000,
 				key: API_KEY,
 			},
 		})
 		.then((response) => {
-            res.json([winningFood, response.data.results]);
+			res.json([winningFood, response.data.results]);
 		})
 		.catch((error) => {
 			res.status(404).send("Unable to find listings");
 		});
+	}
 };
 
 module.exports = { getResults };
